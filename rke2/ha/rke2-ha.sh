cc45f92c8d7120c70e910b31c1618fd3ccd07c1b
Rodney Kusumoto <rtkusumoto@santoandre.sp.gov.br>
2022-10-27 09:26:14 -0300

    RKE2 HA servers

---
+++ rke2/ha/rke2-ha.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Serial: 2022102701
+
+curl -sfL https://get.rke2.io | sh -
+systemctl enable rke2-server.service
+mkdir -p /etc/rancher/rke2
+cp config.yaml /etc/rancher/rke2/
+sleep 10
+systemctl start rke2-server.service
