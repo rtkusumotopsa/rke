38c99e0ef366b86a6dcbfe6159ab1a0527c15ecf
Rodney Kusumoto <rtkusumoto@santoandre.sp.gov.br>
2022-10-27 09:59:39 -0300

    Desabilitado o start automático do serviço

--- rke2/ha/rke2-ha.sh
+++ rke2/ha/rke2-ha.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Serial: 2022102701
+# Serial: 2022102702
 
 curl -sfL https://get.rke2.io | sh -
 systemctl enable rke2-server.service
 mkdir -p /etc/rancher/rke2
 cp config.yaml /etc/rancher/rke2/
-sleep 10
-systemctl start rke2-server.service
+# sleep 10
+# systemctl start rke2-server.service
