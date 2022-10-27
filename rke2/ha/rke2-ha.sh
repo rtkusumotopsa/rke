1098325e22d85d9dc2849683b4b752c68960d2cd
Rodney Kusumoto <rtkusumoto@santoandre.sp.gov.br>
2022-10-27 10:02:59 -0300

    Adicionado parametro do servidor adicional

--- rke2/ha/rke2-ha.sh
+++ rke2/ha/rke2-ha.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Serial: 2022102702
+# Serial: 2022102703
 
 curl -sfL https://get.rke2.io | sh -
 systemctl enable rke2-server.service
 mkdir -p /etc/rancher/rke2
 cp config.yaml /etc/rancher/rke2/
 # sleep 10
-# systemctl start rke2-server.service
+echo "Execute para inicializar: systemctl start rke2-server.service"
